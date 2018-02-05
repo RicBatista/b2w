07acd679625573ce1e1376c27e00155387fc3333
diegoferques <diegoferques@gmail.com>
2018-02-05 14:13:48 -0200

    Criado o Repositorio Planeta

---
+++ src/main/java/com/b2w/desafio/repository/PlanetaRepository.java
@@ -0,0 +1,20 @@
+package com.b2w.desafio.repository;
+
+import com.b2w.desafio.model.Planeta;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Created by matto on 05/02/2018.
+ */
+public interface PlanetaRepository extends MongoRepository<Planeta, String> {
+
+    Planeta findByNome(String nome);
+
+    Planeta findByIdAndNome(String id, String nome);
+
+    /*
+    //Supports native JSON query string
+    @Query("{nome:'?0'}")
+    Planeta findCustomByNome(String nome);
+    */
+}
