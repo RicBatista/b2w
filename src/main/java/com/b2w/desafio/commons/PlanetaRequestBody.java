c4fc5729d8c43c2249eaf5792572fcaf3b2127e0
diegoferques <diegoferques@gmail.com>
2018-02-05 14:26:28 -0200

    Criado o PlanetaRequestBody para receber o Json no Endpoint Criado no Controller

---
+++ src/main/java/com/b2w/desafio/commons/PlanetaRequestBody.java
@@ -0,0 +1,13 @@
+package com.b2w.desafio.commons;
+
+import com.b2w.desafio.model.Planeta;
+import lombok.Data;
+
+/**
+ * Created by matto on 05/02/2018.
+ */
+@Data
+public class PlanetaRequestBody {
+
+    private Planeta planeta;
+}
